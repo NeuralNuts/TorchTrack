275c5b3b1c82702d49312372e9a706598998696d
NeuralNuts <wilsonzakk8@gmail.com>
2023-10-08 00:00:11 +1000

    misc

--- README.md
+++
@@ -1,2 +0,0 @@
-# TrackML_CLI
-The CLI (command line interface) for TrackML web interface
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="track_ml",
-    packages=find_packages(include=["track_ml"]),
+    name="torch_track",
+    packages=find_packages(include=["torch_track"]),
     version="0.1.0",
-    description="TrackML takes your models data and training data then send it to the TrackML web application",
+    description="TorchTrack takes your models data and training data then send it to the TrackML web application",
     author="NeuralNuts",
     install_requires=[],
     setup_requires=['pytest-runner'],
